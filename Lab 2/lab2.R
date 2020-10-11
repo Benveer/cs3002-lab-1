5314b7210ae7566b35ce4c34bb0736c9b33f1729
Benveer <1729793@brunel.ac.uk>
2020-10-11 12:40:47 +0100

    Changed logic slightly

--- Lab 2/lab2.R
+++ Lab 2/lab2.R
@@ -1,6 +1,8 @@
 getwd()
 mydata= read.csv('C:\\Users\\benve\\Documents\\university\\year3\\CS3002\\Labs\\Lab 2\\iris.csv',sep=',')
 
+irisReal= read.csv('C:\\Users\\benve\\Documents\\university\\year3\\CS3002\\Labs\\Lab 2\\iris_real.csv',sep=',')
+
 plot(mydata)
 
 mydata = na.omit(mydata) # deletion of missing data
@@ -12,15 +14,15 @@ source("C:\\Users\\benve\\Documents\\university\\year3\\CS3002\\Labs\\Lab 2\\WK_
 
 iter=20
 
-list=c("single","complete","average")
+list=c("single","complete","average","kmeans")
 
-matrix <- matrix('', iter, 2)
-for(i in 1:length(list))
+matrixHCluster <- matrix('', iter, 2)
+matrixKMeans <- matrix('', iter, 2)
+matrixMeans <- matrix('', iter, 2)
+
+
+for(i in list )
 {
-  
-  print(list[i])
-  
-  
   for(noOfClusters in 2:iter)
   {
     
@@ -28,6 +30,8 @@ for(i in 1:length(list))
     #Average
     
     
+    if(isTRUE(list[i]!= "kmeans"))
+    {    
     fit <- hclust(d, method=list[i])
     #plot(fit) # display dendogram for each iter
     
@@ -36,45 +40,50 @@ for(i in 1:length(list))
     rect.hclust(fit, k=noOfClusters, border="red") 
     #plot(mydata, col=Hgroups) #display scatter plot for each iter of hcluster
     
+    wk = WK_R( Hgroups,irisReal$X1)
     
-    
-    
-    
-    #Kmeans clustering
-    fit <- kmeans(mydata, noOfClusters) # 5 cluster solution
-    
-    aggregate(mydata,by=list(fit$cluster),FUN=mean)
-    
-    Kgroups = fit$cluster 
-    
-    #plot(mydata, col=Kgroups) #display scatter plot for each iter of kcluster
-    
-    
-    
-    
-    wk = WK_R(Kgroups, Hgroups)
-    
-    
-    matrix[noOfClusters,1]<- noOfClusters
-    matrix[noOfClusters,2]<- wk
-    
-    
-    
-    print(paste(list[i],"vs kmeans:"))
+    matrixMeans[noOfClusters,1]<- noOfClusters
+    matrixMeans[noOfClusters,2]<- wk
     print(wk)
     
     
+    }
     
     
     
-    
+    else{
+      
+      
+      #Kmeans clustering
+      fit <- kmeans(mydata, noOfClusters) # 5 cluster solution
+      
+      aggregate(mydata,by=list(fit$cluster),FUN=mean)
+      
+      Kgroups = fit$cluster 
+      
+      #plot(mydata, col=Kgroups) #display scatter plot for each iter of kcluster
+      
+      wk = WK_R( Kgroups,irisReal$X1)
+      
+      matrixMeans[noOfClusters,1]<- noOfClusters
+      matrixMeans[noOfClusters,2]<- wk
+      
+      print(wk)
+      
+      
+      
+    }
+
   }
   
-  plot(matrix,main=list[i],xlab= "Number of Clusters", ylab= "Weight")
+  plot(matrixMeans,main="list[i]",xlab= "Number of Clusters", ylab= "Weight")
   
   
+}
   
-  }  
+
+
+  
 
 
 
